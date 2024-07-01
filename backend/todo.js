5d220419c081b810c2b181e9150823f8383cd73c
Adithiyan-R <adithiyan13r@gmail.com>
2024-07-01 21:54:29 +0530

    modified vercel.json

--- backend/todo.js
+++ backend/todo.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const cors = require('cors');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyparser.json());
