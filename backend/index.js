c86cfc0739f1073b5de44517a5ee2c6814bfbb7d
Adithiyan-R <adithiyan13r@gmail.com>
2024-07-03 10:49:42 +0530

    modified cors

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,13 @@ app.use(
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
     })
   );
+
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
   
 
 const UserSchema = new mongoose.Schema({
